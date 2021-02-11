fe0cec58531a7b8e1d319ca2ecfab2c11fc89c80
Henry Mcgee <henry.mcgee1nz@gmail.com>
2021-02-12 10:55:34 +1300

    Added functionality for decimal points

--- calculator.js
+++ calculator.js
@@ -2,22 +2,46 @@ let screenDisplay = "0";
 let runningTotal = 0;
 let previousOperator = null;
 let newNum = true;
-const screen = document.querySelector(".screen-display");
+const screen = document.querySelector(".screen-num");
 
 document.querySelector(".keypad").addEventListener("click", function (event) {
   buttonClick(event.target.innerText);
 });
 
 function buttonClick(value) {
-  if (isNaN(parseInt(value))) {
+  if (value === ".") {
+    handleDecimalPoint();
+  } else if (isNaN(parseFloat(value))) {
     handleSymbol(value);
   } else {
     handleNumber(value);
+    decreaseFontSize();
   }
   rerender();
 }
 
+function handleDecimalPoint() {
+  if (screenDisplay.slice(-1) === ".") {
+    return;
+  }
+
+  if (screenDisplay === "0" || newNum === true) {
+    if (previousOperator === null) {
+      screenDisplay += ".";
+    } else {
+      screenDisplay = "0.";
+    }
+  } else {
+    screenDisplay += ".";
+  }
+
+  newNum = false;
+}
+
 function handleNumber(num) {
+  if (screenDisplay.length > 35) {
+    reset();
+  }
   if (screenDisplay === "0" || newNum === true) {
     screenDisplay = num;
   } else {
@@ -29,9 +53,7 @@ function handleNumber(num) {
 function handleSymbol(symbol) {
   switch (symbol) {
     case "AC":
-      screenDisplay = "0";
-      runningTotal = 0;
-      previousOperator = null;
+      reset();
       break;
     case "←":
       if (screenDisplay.length === 1) {
@@ -39,29 +61,35 @@ function handleSymbol(symbol) {
       } else {
         screenDisplay = screenDisplay.substring(0, screenDisplay.length - 1);
       }
+      increaseFontSize();
       break;
+    case "%":
+      screenDisplay /= 100;
     case "=":
       if (previousOperator === null) {
         return;
       }
-      computeOperation(parseInt(screenDisplay));
+      computeOperation(parseFloat(screenDisplay));
       newNum = true;
       previousOperator = null;
       runningTotal = 0;
       break;
     default:
+      if (previousOperator === symbol) {
+        return;
+      }
       calculateRunningTotal(symbol);
+      increaseFontSize();
       newNum = true;
   }
 }
 
 function calculateRunningTotal(symbol) {
   if (runningTotal === 0) {
-    runningTotal = parseInt(screenDisplay);
+    runningTotal = parseFloat(screenDisplay);
   } else {
-    computeOperation(parseInt(screenDisplay));
+    computeOperation(parseFloat(screenDisplay));
   }
-  console.log("symbol = " + symbol);
   previousOperator = symbol;
 }
 
@@ -75,9 +103,55 @@ function computeOperation(screenNum) {
   } else {
     runningTotal /= screenNum;
   }
+  runningTotal = runningTotal.toFixed(6);
+  console.log("running total = " + runningTotal);
   screenDisplay = "" + runningTotal;
+  decreaseFontSize();
 }
 
 function rerender() {
-  screen.innerText = screenDisplay;
+  let output;
+
+  if (screenDisplay[screenDisplay.length - 1] === ".") {
+    output = "." + screenDisplay.slice(0, -1);
+  } else {
+    output = screenDisplay;
+  }
+
+  screen.innerText = output;
+}
+
+function reset() {
+  screenDisplay = "0";
+  runningTotal = 0;
+  previousOperator = null;
+  increaseFontSize();
+}
+
+function decreaseFontSize() {
+  const display = document.querySelector(".screen-num");
+
+  let fontSize = window.getComputedStyle(display).getPropertyValue("font-size");
+  fontSize = parseFloat(fontSize.substring(0, fontSize.length - 2));
+
+  while (display.scrollWidth > display.clientWidth && fontSize > 13) {
+    fontSize -= 1;
+    display.style.fontSize = `${fontSize}px`;
+  }
+}
+
+function increaseFontSize() {
+  const display = document.querySelector(".screen-num");
+
+  let fontSize = window.getComputedStyle(display).getPropertyValue("font-size");
+  fontSize = parseFloat(fontSize.substring(0, fontSize.length - 2));
+
+  if (screenDisplay.length == 1) {
+    display.style.fontSize = "60px";
+  }
+
+  while (display.scrollWidth <= display.clientWidth && fontSize < 60) {
+    fontSize += 1;
+    display.style.fontSize = `${fontSize}px`;
+  }
 }
